31b767994edde501a6f5904c473d799012cf1eae
Shandra Morton <ShanMorton@gmail.com>
2024-10-01 20:21:17 -0700

    Add api info and correct api call for PDX.

---
+++ weather.py
@@ -0,0 +1,8 @@
+import resource
+# Import this libraryt to use some of it's functions.
+
+# d29ed9d06fffc17f2715092156ee12eb  Remeber that this would be kept secret in the real world.
+# find a way to hide this
+
+# this is the api call https://api.openweathermap.org/data/2.5/weather?lat=5.5234&lon=-122.6762&appid=d29ed9d06fffc17f2715092156ee12eb
+# use the Lat/Long for PDX  [45.5234, -122.6762]
