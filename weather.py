3c0b57a8553dc688495b0020dc7acbc265892fae
Shandra Morton <ShanMorton@gmail.com>
2024-10-01 21:36:41 -0700

    Everything is working RIGHT NOW!!!

--- weather.py
+++ weather.py
@@ -7,24 +7,8 @@ import requests
 
 # this is the api call https://api.openweathermap.org/data/2.5/weather?lat=5.5234&lon=-122.6762&appid=d29ed9d06fffc17f2715092156ee12eb
 # use the Lat/Long for PDX  [45.5234, -122.6762]
-try:
-    responce = requests.get(f"https://api.openweathermap.org/data/2.5/weather?lat=5.5234&lon=-122.6762&appid=d29ed9d06fffc17f2715092156ee12eb")
-    # fif this cmd runs it'll print good data below
-except:
-    print("This is not working for you at this time!\n")
-    #Other wise I get this print for an error.
 
-responce_json = responce.json()
-# this will put the result in a variable and put it json format, so that it's easy to read.
 
-# To get data from the dictonary
-temperature = responce_json["main"]["temp"]
-#print(temperature)
-temp_min = responce_json["main"]["temp_min"]
-#print(temp_min)
-temp_max = responce_json["main"]["temp_max"]
-#print(temp_max)
-print(f"\n\nIn Portland OR, the temp is {temperature} \u00B0F. The MinTemp is {temp_min}\u00B0F, and the MaxTemp is {temp_max}\u00B0F.\n\n")
 
 class City():
 
@@ -36,8 +20,37 @@ class City():
         self.lat = lat
         self.long = long
         self.units = units
+        #Call the GetData for the city we want to create
+        self.getData()
+
+    def getData(self):
+        try:
+            responce = requests.get(f"https://api.openweathermap.org/data/2.5/weather?units={self.units}&lat={self.lat}&lon={self.long}&appid=d29ed9d06fffc17f2715092156ee12eb")
+            # if this cmd runs it'll print good data below
+        except:
+            print("This is not working for you at this time!\n")
+            #Other wise I get this print for an error.
+
+        responce_json = responce.json()
+        # this will put the result in a variable and put it json format, so that it's easy to read.
+
+        # To get data from the dictonary
+        self.temp = responce_json["main"]["temp"]
+        print(self.temp)
+        self.temp_min = responce_json["main"]["temp_min"]
+        print(self.temp_min)
+        self.temp_max = responce_json["main"]["temp_max"]
+        print(self.temp_max)
+        
+        
+    def temp_print(self):
+        print(f"\n\nIn Portland OR, the temp is {self.temp} \u00B0F. The MinTemp is {self.temp_min}\u00B0F, and the MaxTemp is {self.temp_max}\u00B0F.\n\n")
+
+
 
 #call/create a city object
 # I didn't supply a unit value b/c is has a default at this time, it can be supplied as needed in the call.
-myCity = City("Portland", 5.5234,-122.6762)
 # put the object I call into the variable "myCity"
+myCity = City("Portland", 5.5234,-122.6762)
+myCity.temp_print()
+
