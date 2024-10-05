dbca8980a0e588580fb057ce45422ae7cdceeb26
Shandra Morton <ShanMorton@gmail.com>
2024-10-05 10:20:45 -0700

    Exposed API mostly fixed

--- weatherForC.py
+++ weatherForC.py
@@ -5,7 +5,7 @@ import requests
 # d29ed9d06fffc17f2715092156ee12eb  Remember that this would be kept secret in the real world.
 # find a way to hide this
 
-#this is the api call https://api.openweathermap.org/data/2.5/weather?lat=5.5234&lon=-122.6762&appid=d29ed9d06fffc17f2715092156ee12eb
+#this is the api call https://api.openweathermap.org/data/2.5/weather?lat=5.5234&lon=-122.6762&appid=d29ed9d06fffc17f2715092156ee12e
 #use the Lat/Long for PDX  [45.5234, -122.6762]
 
 ###Challenge 
