4ac85101f19258dd540a28cb071a0f9d65a554f6
ShanMorton <55884638+ShanMorton@users.noreply.github.com>
2024-10-06 10:48:02 -0700

    Update weather.py
    Found another API exposed

--- weather.py
+++ weather.py
@@ -2,7 +2,7 @@ import requests
 
 # Import this library to use some of it's functions.
 
-# d29ed9d06fffc17f2715092156ee12eb  Remember that this would be kept secret in the real world.
+# d29ed9d06fffc17f2715092156ee12e  Remember that this would be kept secret in the real world.
 # find a way to hide this
 
 #this is the api call https://api.openweathermap.org/data/2.5/weather?lat=5.5234&lon=-122.6762&appid=d29ed9d06fffc17f2715092156ee12e
